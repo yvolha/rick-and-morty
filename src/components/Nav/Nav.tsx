17b4ea871cc986b06d2d3c48f363850bfda36fa3
Olya Y <f_akora@mail.ru>
2023-10-15 23:58:43 +0300

    feat: update nav css file not to be a module

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,14 +1,14 @@
 import { NavLink } from "react-router-dom";
 
-import styles from "./Nav.module.css";
+import "./Nav.css";
 
 export default function Nav() {
     return (
-        <div>
-            <NavLink to="/" className={({ isActive }) => (isActive ? `active ${styles.nav_item}` : styles.nav_item)}>Home</NavLink>
-            <NavLink to="/all" className={({ isActive }) => (isActive ? `active ${styles.nav_item}` : styles.nav_item)}>All Chars</NavLink>
-            <NavLink to="/random" className={({ isActive }) => (isActive ? `active ${styles.nav_item}` : styles.nav_item)}>Random Char</NavLink>
-            <NavLink to="/about" className={({ isActive }) => (isActive ? `active ${styles.nav_item}` : styles.nav_item)}>About</NavLink>
-        </div>
+        <nav className="nav">
+            <NavLink to="/" className={({ isActive }) => (isActive ? "active nav_item" : "nav_item")}>Home</NavLink>
+            <NavLink to="/all" className={({ isActive }) => (isActive ? "active nav_item" : "nav_item")}>All Chars</NavLink>
+            <NavLink to="/random" className={({ isActive }) => (isActive ? "active nav_item" : "nav_item")}>Random Char</NavLink>
+            <NavLink to="/about" className={({ isActive }) => (isActive ? "active nav_item" : "nav_item")}>About</NavLink>
+        </nav>
     );
 }
