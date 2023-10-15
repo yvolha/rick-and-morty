49eaea92c745b9174e1a60468b1411374ece7744
Olya Y <f_akora@mail.ru>
2023-10-15 23:52:29 +0300

    feat: add conditional styling to nav items

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,5 +2,5 @@ import { IButtonProps } from "./Button.type";
 import styles from "./Button.module.css";
 
 export default function Button({type, value}: IButtonProps) {
-    return <button type={type} value={value && value} className={styles.app_button}></button>
+    return <button type={type} value={value && value} className={styles.app_button}></button>;
 }
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,16 +1,14 @@
 import { NavLink } from "react-router-dom";
 
-import styling from "./Nav.module.css";
-import Button from "../Button/Button";
+import styles from "./Nav.module.css";
 
 export default function Nav() {
     return (
-        <div className={styling.nav}>
-            <Button type="button" />
-            <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink>
-            <NavLink to="/all" className={({ isActive }) => (isActive ? "active" : "")}>All Chars</NavLink>
-            <NavLink to="/random" className={({ isActive }) => (isActive ? "active" : "")}>Random Char</NavLink>
-            <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}>About</NavLink>
+        <div>
+            <NavLink to="/" className={({ isActive }) => (isActive ? `active ${styles.nav_item}` : styles.nav_item)}>Home</NavLink>
+            <NavLink to="/all" className={({ isActive }) => (isActive ? `active ${styles.nav_item}` : styles.nav_item)}>All Chars</NavLink>
+            <NavLink to="/random" className={({ isActive }) => (isActive ? `active ${styles.nav_item}` : styles.nav_item)}>Random Char</NavLink>
+            <NavLink to="/about" className={({ isActive }) => (isActive ? `active ${styles.nav_item}` : styles.nav_item)}>About</NavLink>
         </div>
     );
 }
