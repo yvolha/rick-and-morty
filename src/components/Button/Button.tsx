1529e6256c63de06c4328c032e6cb0197ad68ac7
Olya Y <f_akora@mail.ru>
2023-10-15 23:17:55 +0300

    feat: add linear gradient transition to a button element

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,6 @@
 import { IButtonProps } from "./Button.type";
+import styles from "./Button.module.css";
 
 export default function Button({type, value}: IButtonProps) {
-    return <button type={type} value={value}></button>
+    return <button type={type} value={value && value} className={styles.app_button}></button>
 }
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
 
 import styling from "./Nav.module.css";
+import Button from "../Button/Button";
 
 export default function Nav() {
     return (
         <div className={styling.nav}>
+            <Button type="button" />
             <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>Home</NavLink>
             <NavLink to="/all" className={({ isActive }) => (isActive ? "active" : "")}>All Chars</NavLink>
             <NavLink to="/random" className={({ isActive }) => (isActive ? "active" : "")}>Random Char</NavLink>
