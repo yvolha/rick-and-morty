1a238aeb8481ddeb84613fdca6fded19f4c0cd97
Olya Y <f_akora@mail.ru>
2023-10-16 21:26:23 +0300

    feat: add logo and hero image options

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
 import Nav from "../Nav/Nav";
 import styles from "./Header.module.css";
 
 export default function Header () {
     return (
         <header className={styles.header}>
+            <Link to={"/"} className={styles.logo_wrapper}>
+                <div className={styles.logo}></div>
+            </Link>
             <Nav />
         </header>
     );
