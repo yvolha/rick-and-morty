2d89ee4f411e24e5d52a837d250d2e0a6acda145
Olya Y <f_akora@mail.ru>
2023-10-16 22:41:12 +0300

    feat: add hero background with randomly picked image

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import "./App.css";
 import appRouter from "./components/Router/Router";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
 
 export function Root() {
     return (
         <>
             <Header />
+            <Hero />
             <main>
                 <Outlet />
             </main>
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,11 +1,17 @@
 import styles from "./Hero.module.css";
 
 export default function Hero(){
-    const pickBackground = () => {
+    const pickBackgroundImage = () => {
         const minBackgroundNum = 1;
         const maxBackgroundNum = 10;
-        const randomBackgroundNum = Math.random()
+        const randomBackgroundNum = Math.floor(Math.random() * (maxBackgroundNum - minBackgroundNum) + minBackgroundNum);
+        console.log(randomBackgroundNum);
+        return `center / cover no-repeat url("./bg/bg-${randomBackgroundNum}.webp")`;
     }
 
-    return <div className={styles.hero}></div>
+    return (
+        <div className={styles.hero} style={{background: pickBackgroundImage()}}>
+            <div className={styles.hero_blur}></div>
+        </div>
+    )
 }
